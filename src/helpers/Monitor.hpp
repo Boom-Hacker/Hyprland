d2e56acb400bc434bb0ed3916b4218473b808a54
Vaxry <43317083+vaxerski@users.noreply.github.com>
2025-08-16 20:02:15 +0100

    renderer: improve zoom in anims (#11453)
    Removes `animations:first_launch_animation` as it's useless now

--- src/helpers/Monitor.hpp
+++ src/helpers/Monitor.hpp
@@ -184,6 +184,9 @@ class CMonitor {
 
     PHLANIMVAR<float> m_cursorZoom;
 
+    // for initial zoom anim
+    PHLANIMVAR<float> m_zoomAnimProgress;
+
     struct {
         bool canTear         = false;
         bool nextRenderTorn  = false;
