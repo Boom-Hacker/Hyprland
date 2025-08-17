6170fe32b0af5f3a5384bfbf99d5bf426859a2a5
Vaxry <43317083+vaxerski@users.noreply.github.com>
2025-08-17 08:37:13 +0100

    renderer: add dpms animations (#11452)

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -1009,6 +1009,7 @@ void CConfigManager::setDefaultAnimationVars() {
     m_animationTree.createNode("fadePopups", "fade");
     m_animationTree.createNode("fadePopupsIn", "fadePopups");
     m_animationTree.createNode("fadePopupsOut", "fadePopups");
+    m_animationTree.createNode("fadeDpms", "fade");
 
     // workspaces
     m_animationTree.createNode("workspacesIn", "workspaces");
--- src/helpers/Monitor.hpp
+++ src/helpers/Monitor.hpp
@@ -182,6 +182,10 @@ class CMonitor {
     // for special fade/blur
     PHLANIMVAR<float> m_specialFade;
 
+    // for dpms off anim
+    PHLANIMVAR<float> m_dpmsBlackOpacity;
+    bool              m_pendingDpmsAnimation = false;
+
     PHLANIMVAR<float> m_cursorZoom;
 
     // for initial zoom anim
@@ -233,6 +237,7 @@ class CMonitor {
     bool                                attemptDirectScanout();
     void                                setCTM(const Mat3x3& ctm);
     void                                onCursorMovedOnMonitor();
+    void                                setDPMS(bool on);
 
     void                                debugLastPresentation(const std::string& message);
 
@@ -260,6 +265,7 @@ class CMonitor {
   private:
     void                    setupDefaultWS(const SMonitorRule&);
     WORKSPACEID             findAvailableDefaultWS();
+    void                    commitDPMSState(bool state);
 
     bool                    m_doneScheduled = false;
     std::stack<WORKSPACEID> m_prevWorkSpaces;
